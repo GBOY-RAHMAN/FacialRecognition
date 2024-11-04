e43cba87ec0ab94a607bc8391ccd87ae806d36ae
soxxerr <159965015+soxxerr@users.noreply.github.com>
2024-11-03 23:52:03 -0500

    #

--- src/FAQ.jsx
+++ src/FAQ.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Faq.css'
+import AddIcon from '@mui/icons-material/Add';
 
 
 export default function FAQ() {
@@ -11,17 +12,17 @@ export default function FAQ() {
   <h1 className='faq_title'>Frequently Asked Questions</h1>
  <div className='faq_dropper'>
   <section className='quote'> 
-    <p className='questions_section'>How does facial recognition payment work? </p>
+    <p className='questions_section'>How does facial recognition payment work? <span className='icons_readmore'> < AddIcon /></span> </p>
     <p className='questions_answer'>Facial recognition payment uses advanced AI algorithms to verify your identity by analyzing your facial features. When you authorize a payment, the system quickly scans your face, matches it with your registered profile, and confirms your identity before processing the transaction.
 
 </p></section>
 <section className='quote'> 
-    <p className='questions_section'>Is my personal data secure? </p>
+    <p className='questions_section'>Is my personal data secure? <span className='icons_readmore'> < AddIcon /></span>  </p>
     <p className='questions_answer'>Yes, we prioritize your security and privacy. Our system encrypts all facial data, ensuring it is protected from unauthorized access. Additionally, your data is stored in compliance with industry standards, keeping your information safe and private.
 
 </p></section>
 <section className='quote'> 
-    <p className='questions_section'>Can someone else use my photo to make a payment? </p>
+    <p className='questions_section'>Can someone else use my photo to make a payment? <span className='icons_readmore'> < AddIcon /></span>  </p>
     <p className='questions_answer'>No, the system is designed to detect and reject attempts to use photos, videos, or masks. It only works with live facial recognition, so your payment can only be authorized when you are present and actively using the system.
 
 </p></section>
