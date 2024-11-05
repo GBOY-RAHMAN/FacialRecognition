392132841b85b03547be5df6d8af890c7e2dd89c
soxxerr <159965015+soxxerr@users.noreply.github.com>
2024-11-05 12:55:12 -0500

    #

--- src/FAQ.jsx
+++ src/FAQ.jsx
@@ -32,25 +32,18 @@ export default function FAQ() {
                 {openIndex === 2 ? <CloseIcon /> : <AddIcon />}
               </span></p>
     
-              {openIndex === 2 && (<p className='questions_answer'>Yes, we prioritize your security and privacy. Our system encrypts all facial data, ensuring it is protected from unauthorized access. Additionally, your data is stored in compliance with industry standards, keeping your information safe and private.
+              {openIndex === 1 && (<p className='questions_answer'>Yes, we prioritize your security and privacy. Our system encrypts all facial data, ensuring it is protected from unauthorized access. Additionally, your data is stored in compliance with industry standards, keeping your information safe and private.
 
 </p>)}</section>
 <section className='quote'> 
     <p className='questions_section'>Can someone else use my photo to make a payment? <span className='icons_readmore' onClick={() => toggleAnswer(3)}>
                 {openIndex === 3 ? <CloseIcon /> : <AddIcon />}
               </span>  </p>
-               <p className='questions_answer'>No, the system is designed to detect and reject attempts to use photos, videos, or masks. It only works with live facial recognition, so your payment can only be authorized when you are present and actively using the system.
+                     <p className='questions_answer'>No, the system is designed to detect and reject attempts to use photos, videos, or masks. It only works with live facial recognition, so your payment can only be authorized when you are present and actively using the system.
 
 </p></section>
 
-<section className='quote'> 
-    <p className='questions_section'>How do I set up facial recognition payment? <span className='icons_readmore' onClick={() => toggleAnswer(3)}>
-                {openIndex === 3 ? <CloseIcon /> : <AddIcon />}
-              </span>  </p>
-               <p className='questions_answer'>How do I set up facial recognition payment?
-Simply download our app (or go to our website), create an account, and follow the steps to set up your facial profile. Once you’ve completed registration, you’ll be able to authorize payments with just a glance.
 
-</p></section>
 </div>
 </section>
     </div>
