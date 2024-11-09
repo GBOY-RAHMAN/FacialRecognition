2984c454a9e5b5e40c8b07d421a26f3dd3bfc6ba
soxxerr <159965015+soxxerr@users.noreply.github.com>
2024-11-09 10:24:48 -0500

    collaraborator

--- src/Motion.jsx
+++ src/Motion.jsx
@@ -10,7 +10,13 @@ import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
 import Typography from '@mui/material/Typography';
 import homebanner from '/homebanner.svg'
-
+import Box from '@mui/material/Box';
+import CircularProgress from '@mui/material/CircularProgress';
+import { green } from '@mui/material/colors';
+import Fab from '@mui/material/Fab';
+import CheckIcon from '@mui/icons-material/Check';
+import SaveIcon from '@mui/icons-material/Save';
+import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
 
 
 const BootstrapDialog = styled(Dialog)(({ theme }) => ({
@@ -23,6 +29,37 @@ const BootstrapDialog = styled(Dialog)(({ theme }) => ({
 }));
 
 export default function Motion() {
+  // Progess javascript
+  const [loading, setLoading] = React.useState(false);
+  const [success, setSuccess] = React.useState(false);
+  const timer = React.useRef(undefined);
+
+  const buttonSx = {
+    ...(success && {
+      bgcolor: green[500],
+      '&:hover': {
+        bgcolor: green[700],
+      },
+    }),
+  };
+
+  React.useEffect(() => {
+    return () => {
+      clearTimeout(timer.current);
+    };
+  }, []);
+
+  const handleButtonClick = () => {
+    if (!loading) {
+      setSuccess(false);
+      setLoading(true);
+      timer.current = setTimeout(() => {
+        setSuccess(true);
+        setLoading(false);
+      }, 2000);
+    }
+  };
+  // 
   const [open, setOpen] = React.useState(false);
 
   const handleClickOpen = () => {
@@ -88,9 +125,53 @@ export default function Motion() {
           </Typography>
         </DialogContent>
         <DialogActions>
-          <Button autoFocus onClick={handleClose}>
-            Payment Completed
-          </Button>
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+      <Box sx={{ m: 1, position: 'relative' }}>
+        <Fab
+          aria-label="save"
+          color="primary"
+          sx={buttonSx}
+          onClick={handleButtonClick}
+        >
+          {success ? <CheckIcon /> : <AdminPanelSettingsIcon />}
+        </Fab>
+        {loading && (
+          <CircularProgress
+            size={68}
+            sx={{
+              color: green[500],
+              position: 'absolute',
+              top: -6,
+              left: -6,
+              zIndex: 1,
+            }}
+          />
+        )}
+      </Box>
+      <Box sx={{ m: 1, position: 'relative' }}>
+        <Button
+          variant="contained"
+          sx={buttonSx}
+          disabled={loading}
+          onClick={handleButtonClick}
+        >
+          Accept Payment
+        </Button>
+        {loading && (
+          <CircularProgress
+            size={24}
+            sx={{
+              color: green[500],
+              position: 'absolute',
+              top: '50%',
+              left: '50%',
+              marginTop: '-12px',
+              marginLeft: '-12px',
+            }}
+          />
+        )}
+      </Box>
+    </Box>
         </DialogActions>
       </BootstrapDialog>
          <div>
@@ -99,8 +180,56 @@ export default function Motion() {
           
           </div>
           </div> 
-
-      
+   
+   {/*  */}
+   <Box sx={{ display: 'flex', alignItems: 'center' }}>
+      <Box sx={{ m: 1, position: 'relative' }}>
+        <Fab
+          aria-label="save"
+          color="primary"
+          sx={buttonSx}
+          onClick={handleButtonClick}
+        >
+          {success ? <CheckIcon /> : <AdminPanelSettingsIcon />}
+        </Fab>
+        {loading && (
+          <CircularProgress
+            size={68}
+            sx={{
+              color: green[500],
+              position: 'absolute',
+              top: -6,
+              left: -6,
+              zIndex: 1,
+            }}
+          />
+        )}
+      </Box>
+      <Box sx={{ m: 1, position: 'relative' }}>
+        <Button
+          variant="contained"
+          sx={buttonSx}
+          disabled={loading}
+          onClick={handleButtonClick}
+        >
+          Accept Payment
+        </Button>
+        {loading && (
+          <CircularProgress
+            size={24}
+            sx={{
+              color: green[500],
+              position: 'absolute',
+              top: '50%',
+              left: '50%',
+              marginTop: '-12px',
+              marginLeft: '-12px',
+            }}
+          />
+        )}
+      </Box>
+    </Box>
+      {/*  */}
     
   </section>
     </React.Fragment>
